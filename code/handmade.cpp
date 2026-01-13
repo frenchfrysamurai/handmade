a83190fb37798306e5436d01d04b22f2b211de50
Lenilio <frenchfrysamurai@gmail.com>
2026-01-13 12:52:16 -0600

    Added emacs setup

--- code/handmade.cpp
+++ code/handmade.cpp
@@ -8,21 +8,8 @@ int CALLBACK WinMain(
     LPSTR lpCmdLine, 
     int nCmdshow)
 {
-  AllocConsole();
 
-  FILE* fp;
-  freopen_s(&fp, "CONOUT$", "w", stdout);
-
-  char unsigned Test;
-  char unsigned *TestPointer;
-
-  TestPointer = &Test;
-
-  Test = 255;
-  Test = Test + 1;
-
-
-  MessageBoxA(0, "Hello Handmade World!", "Handmade", MB_ICONINFORMATION | MB_OK); 
+    WNDCLASS WindowClass = {};
 
 
 
---
+++ misc/emacs.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Emacs\emacs-30.2\bin\runemacs.exe" -q -l C:\handmade\misc\.emacs
