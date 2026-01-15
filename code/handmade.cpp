d29a5d9d087da39e110193a8664ef4521c107264
Emilio <frenchfrysamurai@gmail.com>
2026-01-14 21:06:07 -0600

    A bit more progress on Day 2

--- code/handmade.cpp
+++ code/handmade.cpp
@@ -1,17 +1,61 @@
 #include <windows.h>
 
+LRESULT CALLBACK
+MainWindowCallback(HWND Window,
+                   UINT Message,
+                   WPARAM WParam,
+                   LPARAM LParam)
+{
+    LRESULT Result = 0;
+
+#if 0
+    switch(Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        }break;
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        }break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        }break;
+
+        default:
+        {
+            //        OutputDebugStringA("default\n");
+            Result = ;
+        }break;
+    }
+#endif
+
+    return(Result);
+}
+
+        
+
 int CALLBACK WinMain(
-    HINSTANCE hInstance, 
-    HINSTANCE hPrevInstance, 
-    LPSTR lpCmdLine, 
-    int nCmdshow)
+    HINSTANCE Instance, 
+    HINSTANCE PrevInstance, 
+    LPSTR CommandLine, 
+    int Cmdshow)
 {
 // Stopped at 21:27, Day 2
     WNDCLASS WindowClass = {};
 
     WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
-    WindowClass.lpfnWndProc = ;
-    WindowClass.hInstance = hInstance;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
     //WindowClass.hIcon;
     WindowClass.lpszClassName = "HandmadeHeroWindowClass";
 
